d67adab172a29266049f27623627b7747ad9df8e
jimwu0808 <handsomejim0808@gmail.com>
2019-03-04 19:15:35 +0800

    hw1

--- main.c
+++ main.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int (*p[4])(int x,int y);
 int plus(int a, int b) { return a + b; }
 int minus(int a, int b) { return a - b; }
 int multiply(int a, int b) { return a * b; }
 int divided(int a, int b) { return a / b; }
 int main()
 {
-	int a, c;
-	char b;
-	printf("key a Function \nEX: 1 + 1\n");
-	scanf("%i %c %i", &a, &b, &c);
-	//IF ELSE IF
-	if (b == '+')
-		printf("%d %c %d = %d\n", a, b, c, plus(a, c));
-	else if (b == '-')
-		printf("%d %c %d = %d\n", a, b, c, minus(a, c));
-	else if (b == '*')
-		printf("%d %c %d = %d\n", a, b, c, multiply(a, c));
-	else if (b == '/')
-		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
+
+	p[0] =plus;
+	p[1] =minus;
+	p[2] =multiply;
+	p[3] =divided;	
+
+	int a,b,c;
+	int ans;
+        printf("enter two number:");
+	scanf("%i %i", &a, &c);	//IF ELSE IF
+	printf("0:plus,1:minus,2:multiply,3:divided\n");
+	printf("enter operation:");
+	scanf("%d", &b);
+	ans=(*p[b])(a,c);
+	printf("%d", ans);
+        return 0; 
 }
